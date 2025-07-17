13b42cdf179a5a0ceefdf507b25e0ed0b13604a8
Nandu7582 <nandkishor1000008@gmail.com>
2025-07-17 15:13:52 +0530

    Update broker_api.py

--- app/broker_api.py
+++ app/broker_api.py
@@ -1,6 +1,6 @@
 import os
 import pyotp
-from smartapi.smartConnect import SmartConnect  # ✅ Corrected import
+from smartapi.smartConnect import SmartConnect
 from dotenv import load_dotenv
 
 load_dotenv()
